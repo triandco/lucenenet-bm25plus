9af8063e2b29943d9fc8bec2672ab767042f1929
Tri <tri@triandco.com>
2022-12-08 19:16:29 +1100

    WIP: test lucene

--- src/Library.Tests/Test.fs
+++ src/Library.Tests/Test.fs
@@ -1,15 +1,68 @@
 module Library.Tests
 
 open NUnit.Framework
-open Library
 
-[<SetUp>]
-let Setup () =
-  ()
+type TestCase = { 
+  Query: string 
+  Expect: string
+}
 
-[<Test>]
-let Test1 () =
-  let input = "Dog"
-  let expected = "Hello Dog"
-  let value = Say.hello input
-  Assert.That(value, Is.EqualTo(expected))
+[<TestFixture>]
+type Tests = 
+  [<DefaultValue>] val mutable Rank: Ranker
+
+  [<SetUp>]
+  member this.Setup () =
+    this.Rank <-
+      "doc/not-boring-podcast/"
+      |> System.IO.Directory.GetFiles
+      |> Array.map (fun e -> {
+        Path = e
+        Content = System.IO.File.ReadAllText e
+      })
+      
+    ()
+
+  [<Test>]
+  member this.Test1 () =
+    let tests = [
+      {
+        Query = "3 approaches of Capital"
+        Expect = "capital-and-taste"
+      }
+      {
+        Query = "a controversy when they sold Muslim prayer mats as fun home decor"
+        Expect = "shein-the-tik-tok-ecomerce"
+      }
+      {
+        Query = "Extending excellent taste"
+        Expect = "capital-and-taste"
+      }
+      {
+        Query = "combining usability with flexibility is both incredibly difficult and incredibly rewarding"
+        Expect = "excel-never-dies"
+      }
+      {
+        Query = "Investing in Ethereum"
+        Expect = "own-the-internet"
+      }
+      {
+        Query = "best practices for DAOs"
+        Expect = "the-web3-debate.txt"
+      }
+      {
+        Query = "companies which Not Boring Captial has invested in"
+        Expect = "web3-usecase-of-the-future"
+      }
+      {
+        Query = "Which blockchain ecosystem is thriving and has a lot of offer dApp developer"
+        Expect = "solana-summer"
+      }
+    ]
+    
+    tests |> List.map (fun e ->
+      let result = this.Rank
+      Assert.That(result.Head.Path, Is.EqualTo(e.Expect))) 
+    |> ignore
+    ()
+    
--- src/Library/Library.fs
+++ src/Library/Library.fs
@@ -1,4 +1,63 @@
-﻿namespace Library
+﻿module Library.LuceneRanking
 
-module Say =
-  let hello name = $"Hello {name}"
+open Lucene.Net.Analysis
+open Lucene.Net
+
+type Doc = {
+  Path: string
+  Content: string
+}
+
+type SearchResult = {
+  Path: string
+  Score: float32
+}
+
+module Encoder = 
+  module LuceneKey = 
+    [<Literal>]
+    let Content = "Content"
+    
+    [<Literal>]
+    let Path = "Path"
+
+let buildDirectory (doc: Doc list) = 
+  let version = Util.LuceneVersion.LUCENE_48
+  let analyzer = new Standard.StandardAnalyzer(version)
+
+  let directory = new Store.RAMDirectory()
+  let config = new Index.IndexWriterConfig(version, analyzer)
+  use iwriter = new Index.IndexWriter(directory, config)
+  
+  doc 
+  |> List.map (fun e -> 
+    let doc = new Documents.Document()
+    let field1 = new Documents.Field(Encoder.LuceneKey.Content, e.Content, Documents.TextField.TYPE_STORED)
+    doc.Add(field1)
+    let field2 = new Documents.Field(Encoder.LuceneKey.Path, e.Path, Documents.TextField.TYPE_STORED)
+    doc.Add(field2)
+    doc
+  ) 
+  |> List.iter iwriter.AddDocument 
+
+  directory
+
+let rank (directory:Store.RAMDirectory) (query: string) (count:int)= 
+  let version = Util.LuceneVersion.LUCENE_48
+  let analyzer = new Standard.StandardAnalyzer(version)
+
+  use ireader = Index.DirectoryReader.Open(directory)
+  let isearcher = new Search.IndexSearcher(ireader)
+  let parser = new Lucene.Net.QueryParsers.Classic.QueryParser(version, Encoder.LuceneKey.Content, analyzer)
+  let query = parser.Parse(query)
+  
+  isearcher.Search(query, null, count).ScoreDocs
+  |> Array.map (fun e -> 
+    let t = isearcher.Doc(e.Doc)
+    { 
+      Path = t.Get(Encoder.LuceneKey.Path)
+      Score = e.Score
+    }
+  )
+  
+  
